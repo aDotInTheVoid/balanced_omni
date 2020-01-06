59e8e1a762afc69b56fa625ca53c025de941cb38
Nixon <nixon.emoony@gmail.com>
2020-01-06 01:16:05 +0000

    Test create

--- backend/tasks/tests.py
+++ backend/tasks/tests.py
@@ -41,6 +41,7 @@ class UpdateRest(APITestCase):
         self.assertEqual(task.status_code, 200)
         self.assertEqual(Task.objects.get(pk=task_id).is_done, True)
 
+
 class RestFilter(APITestCase):
     def setUp(self):
         self.user = User.objects.create_user("Foo", "f")
@@ -58,6 +59,38 @@ class RestFilter(APITestCase):
         self.assertEqual(not_done_tasks.json()[0]['name'], 'notdone')
 
 
+class RestCreate(APITestCase):
+    def setUp(self):
+        self.user = User.objects.create_user("Foo", "f")
+        self.prof = Profile.objects.get(user=self.user)
+        self.client.force_authenticate(self.user)
+
+    def test_errors_on_empty(self):
+        r = self.client.post('/api/tasks/')
+        self.assertEqual(r.status_code, 400)
+        j = r.json()
+        errors = j.pop('errors')
+        self.assertEqual(j, {})
+        self.assertEqual(errors['name'], ['This field is required.'])
+        self.assertEqual(errors['due_date'], ['This field is required.'])
+        self.assertEqual(errors['is_done'], ['This field is required.'])
+
+    def test_can_create(self):
+
+        task_json = {'name': 'bax',
+                     'due_date': '2023-02-01', 'is_done': True}
+
+        r = self.client.post('/api/tasks/', task_json)
+        self.assertEqual(r.status_code, 201)
+
+        for i in task_json:
+            self.assertEqual(r.json()[i], task_json[i])
+
+        task = Task.objects.get(pk=r.json()['id'])
+        self.assertEqual(task.name, 'bax')
+        self.assertEqual(task.is_done, True)
+        self.assertEqual(task.author, self.prof)
+        self.assertEqual(task.due_date, date(2023, 2, 1))
 
 class RestRead(APITestCase):
     def setUp(self):
