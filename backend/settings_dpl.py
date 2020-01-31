898c57acc7392548d425a0474aedb0af5ef79d9a
Nixon <nixon.emoony@gmail.com>
2020-01-31 23:15:50 +0000

    new ip

--- backend/settings_dpl.py
+++ backend/settings_dpl.py
@@ -2,5 +2,5 @@ from .settings import *
 import os
 
 STATIC_ROOT = os.path.join(BASE_DIR, "static/")
-ALLOWED_HOSTS.append("40.76.72.252") #CHANGE to maching ip or domain
+ALLOWED_HOSTS.append("40.71.19.82") #CHANGE to maching ip or domain
 DEBUG = False
