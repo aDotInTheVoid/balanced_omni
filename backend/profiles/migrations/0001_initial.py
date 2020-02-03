298061dece63225cbd99fe574335a935df2d5aa9
Nixon <nixon.emoony@gmail.com>
2020-02-03 18:04:27 +0000

    format migrations
    it happended when i fmt'd the frontend because they're the same, so I
    need to commit it so I can have a cleen tree

--- backend/profiles/migrations/0001_initial.py
+++ backend/profiles/migrations/0001_initial.py
@@ -17,10 +17,12 @@ class Migration(migrations.Migration):
         migrations.CreateModel(
             name='Profile',
             fields=[
-                ('id', models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
+                ('id', models.AutoField(auto_created=True,
+                                        primary_key=True, serialize=False, verbose_name='ID')),
                 ('created_at', models.DateTimeField(auto_now_add=True)),
                 ('updated_at', models.DateTimeField(auto_now=True)),
-                ('user', models.OneToOneField(on_delete=django.db.models.deletion.CASCADE, to=settings.AUTH_USER_MODEL)),
+                ('user', models.OneToOneField(
+                    on_delete=django.db.models.deletion.CASCADE, to=settings.AUTH_USER_MODEL)),
             ],
             options={
                 'ordering': ['-created_at', '-updated_at'],
