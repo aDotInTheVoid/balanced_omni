bb660f33a3524189479b4c4dba5406324c4c8344
Nixon <nixon.emoony@gmail.com>
2020-02-10 17:42:15 +0000

    new app ui

--- src/App.vue
+++ src/App.vue
@@ -1,40 +1,140 @@
+
 <template>
+  <!-- Based on the google contacts demo -->
+  <!-- https://github.com/vuetifyjs/vuetify/blob/master/packages/docs/src/layouts/layouts/demos/google-contacts.vue -->
+  <!-- https://vuetifyjs.com/en/examples/layouts/google-contacts -->
   <v-app>
-    <v-app-bar
+    <!-- <v-navigation-drawer
+      v-model="drawer"
+      :clipped="$vuetify.breakpoint.lgAndUp"
       app
-      color="primary"
+    >
+      <v-list dense>
+        <template v-for="item in items">
+          <v-row
+            v-if="item.heading"
+            :key="item.heading"
+            align="center"
+          >
+            <v-col cols="6">
+              <v-subheader v-if="item.heading">
+                {{ item.heading }}
+              </v-subheader>
+            </v-col>
+            <v-col
+              cols="6"
+              class="text-center"
+            >
+              <a
+                href="#!"
+                class="body-2 black--text"
+              >EDIT</a>
+            </v-col>
+          </v-row>
+          <v-list-group
+            v-else-if="item.children"
+            :key="item.text"
+            v-model="item.model"
+            :prepend-icon="item.model ? item.icon : item['icon-alt']"
+            append-icon=""
+          >
+            <template v-slot:activator>
+              <v-list-item-content>
+                <v-list-item-title>
+                  {{ item.text }}
+                </v-list-item-title>
+              </v-list-item-content>
+            </template>
+            <v-list-item
+              v-for="(child, i) in item.children"
+              :key="i"
+              link
+            >
+              <v-list-item-action v-if="child.icon">
+                <v-icon>{{ child.icon }}</v-icon>
+              </v-list-item-action>
+              <v-list-item-content>
+                <v-list-item-title>
+                  {{ child.text }}
+                </v-list-item-title>
+              </v-list-item-content>
+            </v-list-item>
+          </v-list-group>
+          <v-list-item
+            v-else
+            :key="item.text"
+            link
+          >
+            <v-list-item-action>
+              <v-icon>{{ item.icon }}</v-icon>
+            </v-list-item-action>
+            <v-list-item-content>
+              <v-list-item-title>
+                {{ item.text }}
+              </v-list-item-title>
+            </v-list-item-content>
+          </v-list-item>
+        </template>
+      </v-list>
+    </v-navigation-drawer> -->
+    <v-app-bar
+      :clipped-left="$vuetify.breakpoint.lgAndUp"
+      app
+      color="blue darken-3"
       dark
     >
-      <div class="d-flex align-center">
-        <router-link
-          to="/"
+      <!-- <v-app-bar-nav-icon @click.stop="drawer = !drawer" /> -->
+      <v-toolbar-title
+        style="width: 300px"
+        class="ml-0 pl-4"
+      >
+        <span class="hidden-sm-and-down">Balanced</span>
+      </v-toolbar-title>
+      <!-- <v-text-field
+        flat
+        solo-inverted
+        hide-details
+        prepend-inner-icon="mdi-magnify"
+        label="Search"
+        class="hidden-sm-and-down"
+      /> -->
+      <v-spacer />
+      <!-- <v-btn icon to="/">
+        <v-icon>mdi-home</v-icon>
+      </v-btn>
+
+      <v-btn icon to="/create">
+        <v-icon>mdi-file-document-box-plus</v-icon>
+      </v-btn> -->
+      <v-tooltip
+        v-for="item in items"
+        :key="item.text"
+        bottom
+      >
+        <template v-slot:activator="{ on }">
+          <v-btn
+            icon
+            :to="item.link"
+            v-on="on"
+          >
+            <v-icon>{{ item.icon }}</v-icon>
+          </v-btn>
+        </template>
+        <span>{{ item.text }}</span>
+      </v-tooltip>
+      <v-btn
+        icon
+        large
+      >
+        <v-avatar
+          size="32px"
+          item
         >
           <v-img
-            alt="Vuetify Logo"
-            class="shrink mr-2"
-            contain
-            src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
-            transition="scale-transition"
-            width="40"
+            src="https://cdn.vuetifyjs.com/images/logos/logo.svg"
+            alt="Vuetify"
           />
-        </router-link>
-      </div>
-      <v-toolbar-title v-text="title" />
-      <v-spacer />
-
-
-      <v-btn
-        icon
-        x-large
-        to="/tasks"
-      >
-        <v-icon>mdi-view-list</v-icon>
-      </v-btn>
-      <v-btn
-        icon
-        x-large
-      >
-        <v-icon>mdi-plus-box</v-icon>
+        </v-avatar>
       </v-btn>
     </v-app-bar>
 
@@ -65,7 +165,17 @@ export default Vue.extend({
 
 
   data() {
-    return { loading: false };
+    return {
+      loading: false,
+      dialog: false,
+      drawer: null,
+      items: [
+        { icon: 'mdi-home', text: 'Home', link: '/' },
+        { icon: 'mdi-file-document-box-plus', text: 'Create Task', link: '/create' },
+        { icon: 'mdi-view-list', text: 'Task List' },
+        { icon: 'mdi-view-grid', text: 'Task Matrix' },
+      ],
+    };
   },
   computed: {
     title() {
