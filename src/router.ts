c7bb0e827ae382857289f288aa642542839767d5
Nixon <nixon.emoony@gmail.com>
2020-03-04 14:03:56 +0000

    router login redirects
    This took me way to long.
    (see recusion comments)

--- src/router.ts
+++ src/router.ts
@@ -1,5 +1,6 @@
 import Vue from 'vue';
-import VueRouter from 'vue-router';
+import VueRouter, { Route } from 'vue-router';
+// import { getToken } from '@/api/jwt';
 
 Vue.use(VueRouter);
 
@@ -13,6 +14,10 @@ const routes = [
     path: '/login',
     name: 'Log in',
     component: () => import(/* webpackChunkName: "login" */'@/views/auth/Login.vue'),
+    meta: { guestOnly: true },
+    props: (route: Route) => ({
+      to: route.query.to || '/',
+    }),
   },
   {
     path: '/tasks',
@@ -46,5 +51,33 @@ const router = new VueRouter({
   base: process.env.BASE_URL,
   routes,
 });
+
+// TODO: make this a call to the API service
+const isAuthenticated = true;
+
+// Beware, it's super easy to make this recurse infinitely.
+// Make sure each path through the code calls `next` exactly once.
+// Also make sure you eventually resolve to a call to `next()` with
+// no arguments.
+router.beforeEach((to, from, next) => {
+  // Accessing a guestOnly (eg login)
+  if (to.matched.some(x => x.meta.guestOnly)) {
+    // If they have auth, go home
+    if (isAuthenticated) {
+      next('/');
+      return;
+    }
+  } else {
+    // Otherwise we require login
+    if (!isAuthenticated) {
+      const loginPath = encodeURIComponent(to.fullPath);
+      next(`/login?to=${loginPath}`);
+      return;
+    }
+  }
+  // By default, just continue
+  next();
+});
+
 
 export default router;
